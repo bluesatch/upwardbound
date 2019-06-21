e2a317f1d412be95e43f816116253c1fb882d12e
Satchel Page <satchelpage@SatchBookPro2.local>
2019-06-21 14:56:45 -0500

    first commit

---
+++ ubstore.js
@@ -0,0 +1,55 @@
+// var greeting = 'Hello World';
+// console.log(greeting);
+
+// var dataStringSentence = "This is a string"; //this is a variable
+
+var dataInteger = 1;
+var dataInteger2 = 2;
+
+
+
+// var a = 1;
+
+console.log(sum);
+// console.log(product);
+
+var sum = dataInteger - dataInteger2;
+var product = dataInteger * dataInteger2;
+
+var thisIsTrue = true;
+var thisIsFalse = false;
+
+if(sum !== -1){
+    console.log(thisIsTrue);
+} else {
+    console.log(thisIsFalse);
+}
+
+// An array is a list of things like strings, numbers, or booleans. You can put an array inside of an array.
+
+
+var thisIsAnArray = ['thing', 1, true, ['thing2', false]]
+
+console.log(thisIsAnArray);
+
+var groceryList = ["eggs", "milk", "cereal", "ice cream", "snickers", "sausages", "bacon", "cheese", "bread", "mustard", "ketchup"];
+
+console.log(groceryList.length);
+
+console.log(groceryList[6]);
+
+console.log(groceryList.push("whip cream"));
+console.log(groceryList);
+
+console.log(groceryList[11]);
+
+console.log(groceryList.reverse());
+
+console.log(groceryList.sort());
+
+// console.log(groceryList.forEach());
+
+
+
+document.write("<h1>This was created using JavaScript</h1>");
+
